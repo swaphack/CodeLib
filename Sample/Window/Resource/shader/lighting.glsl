0bb289a1435e1d19a011aec07eb5e6b78090df4a
swaphack <809406730@qq.com>
2020-04-29 23:41:52 +0800

    add glsl

---
+++ Sample/Window/Resource/shader/lighting.glsl
@@ -0,0 +1,15 @@
+﻿#version 450 core
+
+subroutine vec4 LightFunc(vec3);
+
+subroutine (LightFunc) vec4 ambient(vec3 n)
+{
+	return Materials.ambient;
+}
+
+subroutine (LightFunc) vec4 diffuse(vec3 n)
+{
+	return Materials.diffuse * max(dot(normalize(n), LightFunc.xyz), 0.0);
+}
+
+subroutine uniform LightFunc materialShader;
