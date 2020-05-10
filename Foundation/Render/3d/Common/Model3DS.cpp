02a19ee5afc376bf226fb81dc4654dfd2ea4160b
swaphack <809406730@qq.com>
2020-05-10 22:38:02 +0800

    update

--- Foundation/Render/3d/Common/Model3DS.cpp
+++ Foundation/Render/3d/Common/Model3DS.cpp
@@ -14,12 +14,12 @@ Model3DS::~Model3DS()
 
 void Model3DS::load(const std::string& filepath)
 {
-	//auto task = std::thread([this, filepath](){
+	auto task = std::thread([this, filepath](){
 		File3DS* pFile = Loader::load<File3DS>(filepath);
 		this->setModelData(pFile);
 		SAFE_DELETE(pFile);
-	//});
-	//task.detach();
+	});
+	task.detach();
 }
 /*
 void Model3DS::draw()
